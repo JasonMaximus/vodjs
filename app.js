5cd1cf83ccc90e77f72829411703defd288714e1
callvgroup <dev@callvgroup.net>
2024-06-20 04:11:37 +0700

    [Update] Main File Flow

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * vod.js
+ * 
+ * (C) Callv Group 2024
+ */
+
+'use strict'
+
+module.exports = require("./vodjs/vod");
---
+++ vodjs/vod.js
@@ -0,0 +1,13 @@
+/**
+ * vod.js
+ * 
+ * (C) Callv Group 2024
+ */
+
+class Vod {
+
+    constructor(param) {}
+
+}
+
+module.exports = { Vod };
